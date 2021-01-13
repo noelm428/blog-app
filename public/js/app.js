9af53e21e98c635eca3829ef605646c6f11655a9
Ryan Fanelli <illenafnayr@gmail.com>
2021-01-13 16:39:54 -0500

    Hello World Renders

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+class App extends React.Component {
+    render = ()=>{
+        return <div>
+            <h1>Create a Post!</h1>
+        </div>
+    }
+}
+
+ReactDOM.render(
+    <App></App>,
+    document.querySelector('main')
+)
